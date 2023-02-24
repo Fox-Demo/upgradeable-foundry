8d0dd2c0d0f6a79ec0d7ee0b0d4ee6003ebb25ed
LI-YONG-QI <a2369028@gmail.com>
2023-02-24 16:24:51 +0800

    init commit

---
+++ script/Transparent.s.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+import "forge-std/Script.sol";
+import {Box, BoxV2, IBox} from "../src/Box.sol";
+import {TransparentUpgradeableProxy} from "../src/Transparent.sol";
+
+contract Deploy is Script{
+  uint256 userPrivateKey = vm.envUint("USER_KEY");
+
+  TransparentUpgradeableProxy proxy;
+  Box impl;
+  BoxV2 impl2;
+
+  function _setUp() internal{
+    impl = new Box();
+    impl2 = new BoxV2();
+    bytes memory data = abi.encodeWithSignature("initialize(uint256)", 1);
+    proxy = new TransparentUpgradeableProxy(address(impl), address(this), data);
+  }
+
+  function run() external {
+    _setUp();
+
+    _consoleVersion(userPrivateKey); // version --> 1
+    _upgrade(address(impl2));
+    console.log("Upgrade Success!!");
+    _consoleVersion(userPrivateKey); // version --> 2
+  }
+
+  function _upgrade(address newImpl) internal {
+    proxy.upgradeTo(address(newImpl));
+  }
+
+  function _consoleVersion(uint256 privateKey) internal{
+    vm.startBroadcast(privateKey);
+    console.log(IBox(address(proxy)).getVersion());
+    vm.stopBroadcast();
+  }
+
+
+  
+}
+
+
---
+++ src/Box.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+import "openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
+
+interface IBox {
+    function getVersion() external returns(string memory);
+}
+
+contract Box is IBox, Initializable {
+    uint256 num;
+
+    function initialize(uint256 _value) external initializer {
+        num = _value;
+    }
+
+    function getVersion() external pure returns(string memory){
+        return "Box version --> 1";
+    }
+}
+
+contract BoxV2 is IBox, Initializable {
+    uint256 num;
+
+    function initialize(uint256 _value) external reinitializer(2) {
+        num = _value;
+    }
+
+    function getVersion() external pure returns(string memory){
+        return "Box version --> 2";
+    }
+}
